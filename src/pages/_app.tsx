5a35caaf6a461cb4771bf603644c36b77cce8ba8
dekguh <kdkteguhmahesa@gmail.com>
2023-04-02 17:42:25 +0800

    chore: add StyledEngineProvider

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,17 +1,18 @@
 import React from 'react'
-import '@/styles/globals.css'
 import type { AppProps } from 'next/app'
 
 // MUIS
-import { ThemeProvider } from '@mui/material/styles'
+import { StyledEngineProvider, ThemeProvider } from '@mui/material/styles'
 
 // THEME
 import theme from '../styles/theme'
 
 export default function App({ Component, pageProps }: AppProps) {
   return (
-    <ThemeProvider theme={theme}>
-      <Component {...pageProps} />
-    </ThemeProvider>
+    <StyledEngineProvider injectFirst>
+      <ThemeProvider theme={theme}>
+        <Component {...pageProps} />
+      </ThemeProvider>
+    </StyledEngineProvider>
   )
 }
