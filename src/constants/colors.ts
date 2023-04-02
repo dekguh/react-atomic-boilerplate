c4a057d7dbe44dd4d585f824fa1e44bb75e7d55a
dekguh <kdkteguhmahesa@gmail.com>
2023-04-02 17:38:26 +0800

    chore: create constant colors

---
+++ src/constants/colors.ts
@@ -0,0 +1,39 @@
+const colors = {
+  primary: {
+    main: '#D29E19',
+    light: '#FFD873',
+    dark: '#B7912E',
+  },
+  secondary: {
+    main: '#424242',
+    light: '#606060',
+    dark: '#3A3A3A',
+  },
+  accent: {
+    main: '#82B1FF',
+    light: '#A7C8FF',
+    dark: '#568CE5'
+  },
+  error: {
+    main: '#FF5252',
+    light: '#FF7575',
+    dark: '#D01616',
+  },
+  success: {
+    main: '#4CAF50',
+    light: '#64E969',
+    dark: '#23A227',
+  },
+  info: {
+    main: '#2196F3',
+    light: '#47ADFE',
+    dark: '#167FD3',
+  },
+  warning: {
+    main: '#FB8C00',
+    light: '#FFA83B',
+    dark: '#DC800E',
+  }
+}
+
+export default colors
