9437e82841c1486c1f9168ce72bacddee7f8e435
dekguh <kdkteguhmahesa@gmail.com>
2023-04-22 11:18:58 +0800

    feat: string utils hex to rgb

---
+++ src/utils/string.ts
@@ -0,0 +1,6 @@
+export const hexToRgb = (hex: string) : string | null => {
+  const result = /^#?([a-f\d]{2})([a-f\d]{2})([a-f\d]{2})$/i.exec(hex)
+  return result
+    ? `rgb(${parseInt(result[1], 16)}, ${parseInt(result[2], 16)}, ${parseInt(result[3], 16)})`
+    : null
+}
