2bf901fc4964f05f2bdecd33bed73f16116869ba
dekguh <kdkteguhmahesa@gmail.com>
2023-04-24 11:02:24 +0800

    chore: add contrast text

--- src/constants/colors.ts
+++ src/constants/colors.ts
@@ -3,36 +3,43 @@ const colors = {
     main: '#D29E19',
     light: '#FFD873',
     dark: '#B7912E',
+    contrastText: '#FFFFFF',
   },
   secondary: {
     main: '#424242',
     light: '#606060',
     dark: '#3A3A3A',
+    contrastText: '#FFFFFF',
   },
   accent: {
     main: '#82B1FF',
     light: '#A7C8FF',
-    dark: '#568CE5'
+    dark: '#568CE5',
+    contrastText: '#FFFFFF',
   },
   error: {
     main: '#FF5252',
-    light: '#FF7575',
+    light: '#FFAAAA',
     dark: '#D01616',
+    contrastText: '#FFFFFF',
   },
   success: {
     main: '#4CAF50',
-    light: '#64E969',
+    light: '#AEF7B1',
     dark: '#23A227',
+    contrastText: '#FFFFFF',
   },
   info: {
     main: '#2196F3',
     light: '#47ADFE',
     dark: '#167FD3',
+    contrastText: '#FFFFFF',
   },
   warning: {
     main: '#FB8C00',
     light: '#FFA83B',
     dark: '#DC800E',
+    contrastText: '#FFFFFF',
   }
 }
 
