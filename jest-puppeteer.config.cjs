3318f573d003a7a62e658b4f500c75b5971958b4
dekguh <kdkteguhmahesa@gmail.com>
2023-07-20 16:34:21 +0800

    fix: fix config

--- jest-puppeteer.config.cjs
+++ jest-puppeteer.config.cjs
@@ -1,17 +1,18 @@
 /** @type {import('jest-environment-puppeteer').JestPuppeteerConfig} */
 module.exports = {
   launch: {
+    executablePath: '/usr/bin/google-chrome-stable',
     headless: 'new',
-    ignoreDefaultArgs: ['--disable-extensions'],
-    args: ['--no-sandbox'],
-    dumpio: true
+    args: [
+      '--ignore-certificate-errors',
+      '--no-sandbox',
+      '--disable-setuid-sandbox',
+      '--disable-accelerated-2d-canvas',
+      '--disable-gpu'
+    ]
   },
-  browser: 'chromium',
-  browserContext: 'default',
   server: {
     command: 'npm start',
     port: 3000,
-    launchTimeout: 10000,
-    debug: true,
   },
 }
