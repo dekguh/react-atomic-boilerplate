0cd82cd7fb9219593869696b9c5f784300a30f28
dekguh <kdkteguhmahesa@gmail.com>
2023-07-20 15:31:31 +0800

    fix: change port jest puppeteer server

--- jest-puppeteer.config.cjs
+++ jest-puppeteer.config.cjs
@@ -4,13 +4,14 @@ module.exports = {
     headless: 'new',
     ignoreDefaultArgs: ['--disable-extensions'],
     args: ['--no-sandbox'],
+    dumpio: true
   },
   browser: 'chromium',
   browserContext: 'default',
   server: {
     command: 'npm start',
-    port: 3000,
-    launchTimeout: 120000,
+    port: 3010,
+    launchTimeout: 10000,
     debug: true,
   },
 }
