96cbcb1b4212012524adc42b59a636030ce78160
dekguh <kdkteguhmahesa@gmail.com>
2023-07-20 13:02:21 +0800

    chore: config e2e testing for CI

--- jest-puppeteer.config.cjs
+++ jest-puppeteer.config.cjs
@@ -3,4 +3,12 @@ module.exports = {
   launch: {
     headless: false,
   },
+  browser: 'chromium',
+  browserContext: 'default',
+  server: {
+    command: 'npm start',
+    port: 3000,
+    launchTimeout: 10000,
+    debug: true
+  }
 }
