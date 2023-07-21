85d1e811efb3856a6ef18b60dcbfa818f3e6018a
dekguh <kdkteguhmahesa@gmail.com>
2023-07-21 09:53:06 +0800

    chore: update config jest

--- jest-puppeteer.config.cjs
+++ jest-puppeteer.config.cjs
@@ -2,5 +2,11 @@
 module.exports = {
   launch: {
     headless: 'new',
+    ignoreDefaultArgs: ['--disable-extensions'],
+    args: ['--no-sandbox']
   },
+  server: {
+    command: 'npm run start',
+    port: 3000
+  }
 }
