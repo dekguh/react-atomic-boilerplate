c49508c3adf62a43d6b9e55160aa2c9542965b2f
dekguh <kdkteguhmahesa@gmail.com>
2023-08-04 10:55:02 +0800

    chore: setup config jest puppeteer CI

--- jest-puppeteer.config.cjs
+++ jest-puppeteer.config.cjs
@@ -1,12 +1,24 @@
 /** @type {import('jest-environment-puppeteer').JestPuppeteerConfig} */
-module.exports = {
-  launch: {
-    headless: 'new',
-    ignoreDefaultArgs: ['--disable-extensions'],
-    args: ['--no-sandbox']
-  },
-  server: {
-    command: 'npm run start',
-    port: 3000
-  }
+
+const confServer = {
+  command: 'npm run start',
+  port: 9999
 }
+
+const confLaunch = {
+  headless: 'new',
+  ignoreDefaultArgs: ['--disable-extensions'],
+  args: [
+    '--ignore-certificate-errors',
+    '--no-sandbox',
+    '--disable-setuid-sandbox',
+    '--disable-accelerated-2d-canvas',
+    '--disable-gpu'
+  ]
+}
+
+// CI true = CI on github actions
+module.exports = process.env.CI ? {
+  launch: confLaunch,
+  server: confServer
+} : { launch: confLaunch }
