e89121d85a7e04276ebd6b02fb7b77428d3c0913
dekguh <kdkteguhmahesa@gmail.com>
2023-09-24 21:16:08 +0800

    feat: snackbar slice

---
+++ src/store/slice/snackbarSlice.ts
@@ -0,0 +1,44 @@
+import { PayloadAction, createAction, createSlice } from '@reduxjs/toolkit'
+import { AppState } from '../store'
+import { HYDRATE } from 'next-redux-wrapper'
+
+export interface ISnackbarState {
+  isOpen: boolean;
+  severity: 'success' | 'error' | 'warning' | 'info';
+  title?: string;
+  message: string;
+}
+
+export const initialState : ISnackbarState = {
+  isOpen: false,
+  severity: 'error',
+  title: '',
+  message: ''
+}
+
+const hydrate = createAction<AppState>(HYDRATE)
+
+export const snackbarSlice = createSlice({
+  name: 'snackbar',
+  initialState,
+  reducers: {
+    callSnackbar: (state, action : PayloadAction<ISnackbarState>) => {
+      state = action.payload
+    },
+    setIsOpenSnackbar: (state, action: PayloadAction<boolean>) => {
+      state.isOpen = action.payload
+    }
+  },
+  extraReducers: builder => {
+    builder.addCase(hydrate, (state, action) => {
+      return {
+        ...state,
+        ...action.payload.auth
+      }
+    })
+  },
+})
+
+export const { callSnackbar, setIsOpenSnackbar } = snackbarSlice.actions
+export const selectSnackbarState = (state: AppState) => state.snackbar
+export default snackbarSlice.reducer
--- src/store/store.ts
+++ src/store/store.ts
@@ -3,12 +3,14 @@ import { createWrapper } from 'next-redux-wrapper'
 import { authSlice } from './slice/authSlice'
 import { userSlice}  from './slice/userSlice'
 import { apiSlice } from './slice/apiSlice'
+import { snackbarSlice } from './slice/snackbarSlice'
 
 const makeStore = () => configureStore({
   reducer: {
     [authSlice.name]: authSlice.reducer,
     [userSlice.name]: userSlice.reducer,
-    [apiSlice.reducerPath]: apiSlice.reducer
+    [apiSlice.reducerPath]: apiSlice.reducer,
+    [snackbarSlice.name]: snackbarSlice.reducer
   },
   devTools: process.env.NODE_ENV === 'development' ? true : false,
   middleware: getDefaultMiddleware => getDefaultMiddleware().concat(apiSlice.middleware)
